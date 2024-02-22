7fa4fa3f84cd3bcd917fda0f4aa1e5211bbedd2d
DanielLtg1 <133992780+DanielLtg1@users.noreply.github.com>
2024-02-22 15:39:22 -0300

    Commit - 01
    Adicionando os aquivos via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+alert("Seja bem-vindo!")
+
+document.getElementById("gosto").innerHTML = "O professor facilita também, rs...";
+document.getElementById("nao_gosto").innerHTML = "Preciso dominar e passar";    
+
+console.log("Você não pode me ver!")
